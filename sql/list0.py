d65fdab6929e02f5f833b1ed7717716de5eb0290
boy07132004 <boy07132004@gmail.com>
2019-09-14 02:13:23 +0800

    0914

--- sql/list0.py
+++ sql/list0.py
@@ -14,16 +14,16 @@ def main():
         print(f"{flight.origin} to {flight.destination} lasting {flight.duration}minutes.")
     if True:
         print('===================')
-        print('select * from flights where  ID = 10;')
-        print(Flight.query.filter_by(id=10).first())
-        print(Flight.query.get(10))
+        print('select * from flights where  ID = 2;')
+        print(Flight.query.filter_by(id=2).first())
+        print(Flight.query.get(2))
         print('===================\n')
     if True:
         print('===================')
-        print(Flight.query.filter_by(id=10).first().duration)
-        print('UPDATE flights SET duration=710 where  ID = 10;')
-        flight.query.get(10).duration = 710
-        print(Flight.query.get(10).duration)
+        print(Flight.query.filter_by(id=2).first().duration)
+        print('UPDATE flights SET duration=710 where  ID = 2;')
+        flight.query.get(2).duration = 710
+        print(Flight.query.get(2).duration)
         print('===================\n')
     if True:
         print('===================')
@@ -36,6 +36,13 @@ def main():
         print('===================')
         print('select * from flights WHERE origin!=Korea;')
         print(Flight.query.filter(Flight.origin!='Korea').all())
+        print('===================\n')
+    if True:
+        print('===================')
+        print('select * from flights join passengers on flights.id = passengers.flights_id;')
+        B=db.session.query(Flight,Passenger).filter(Flight.id == Passenger.flight_id).order_by(Flight.id).all()
+        for b in B:
+            print(f"{b.Flight.origin} to {b.Flight.destination} Passengers:{b.Passenger.name}")
         print('===================\n')
     #commit
     #db.session.commit()
